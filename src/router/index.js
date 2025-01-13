2c63c9cd94af170a8af38e9a7640810911adaf5d
Huanswurst <117086200+Huanswurst@users.noreply.github.com>
2025-01-13 15:44:22 +0800

    Update index.js
    暂时修改跳转路由，先测试跑通

--- src/router/index.js
+++ src/router/index.js
@@ -4,28 +4,28 @@ import Register from '../views/Register.vue';
 
 // 学生界面
 import StudentDashboard from '../views/student/dashboard.vue';
-import StudentAttendance from '../views/student/Attendance.vue';
+/*import StudentAttendance from '../views/student/Attendance.vue';
 import StudentCheckIn from '../views/student/CheckIn.vue';
 import StudentCheckOut from '../views/student/CheckOut.vue';
-
+*/
 // 教师界面
 import TeacherDashboard from '../views/teacher/dashboard.vue';
-import CourseAttendance from '../views/teacher/CourseAttendance.vue';
+/*import CourseAttendance from '../views/teacher/CourseAttendance.vue';
 import ManageCourses from '../views/teacher/ManageCourses.vue';
-
+*/
 const routes = [
     { path: '/', redirect: '/login' },
     { path: '/login', component: Login },
     { path: '/register', component: Register },
     // 学生路由
     { path: '/student/dashboard', component: StudentDashboard },
-    { path: '/student/attendance', component: StudentAttendance },
+    /*{ path: '/student/attendance', component: StudentAttendance },
     { path: '/student/checkin', component: StudentCheckIn },
-    { path: '/student/checkout', component: StudentCheckOut },
+    { path: '/student/checkout', component: StudentCheckOut },*/
     // 教师路由
     { path: '/teacher/dashboard', component: TeacherDashboard },
-    { path: '/teacher/course-attendance', component: CourseAttendance },
-    { path: '/teacher/manage-courses', component: ManageCourses },
+    /*{ path: '/teacher/course-attendance', component: CourseAttendance },
+    { path: '/teacher/manage-courses', component: ManageCourses },*/
 ];
 
 const router = createRouter({
