25b081555e437838bebd2a2f9b693e394c2a403d
huanswurst <huanswurst1@outlook.com>
2025-01-13 19:33:37 +0800

    Merge remote changes

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router'; // 确保导入路由
+import ElementPlus from 'element-plus';
+import 'element-plus/dist/index.css';
+
 
 const app = createApp(App);
+app.use(ElementPlus);
 app.use(router); // 使用路由
 app.mount('#app');
